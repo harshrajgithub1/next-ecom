f77e519795a4fbb07a70a4e7c3115f79113e6f69
harshraj.sam@gmail.com <harshraj.sam@gmail.com>
2023-10-10 10:59:46 +0530

    first commit

---
+++ app/components/About/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function hero() {
+  return (
+    <div>
+      <section id="about-2" className="rel inner-page-hero about-section division">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-11 col-lg-10 col-xl-9">
+              <div className="about-2-title mb-60">
+                <h2 className="s-52 w-700 mb-30">Welcome to <span>Trisfusa</span></h2>
+                <p>Trisfusa: Ihr ultimativer Begleiter für Smarthome-Geräte und Automationstechnologien! Entdecken,
+                  vergleichen und finden Sie die perfekten Lösungen für Ihr Smarthome und Ihre Automatisierungsbedürfnisse. </p>
+
+                <p>Mit Trisfusa treffen Sie informierte Entscheidungen, vereinfachen Ihre Heimautomatisierungsreise und
+                  gestalten eine intelligentere Zukunft. Unsere Spezialisierung liegt in der Überprüfung von Kompatibilität
+                  und Schnittstellen, um sicherzustellen, dass alle Ihre Geräte nahtlos miteinander arbeiten.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
---
+++ app/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <section class="banner" style="background-image:url(assets/img/banner6.jpg)">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="banner-caption">
+                    <h3>About us</h3>
+                    <ul class="breadcrumb">
+                        <li><a href="index.php">Start</a></li>
+                        <li>About us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    </div>
+  )
+}
